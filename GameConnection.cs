26277dc5edc11ae3a73d6695151ce6bfd343bb1b
PhongVan <fmlphongvan@gmail.com>
2022-11-23 01:34:23 +0700

    Handle game connection when it has too many clients/or a client left match

--- GameConnection.cs
+++ GameConnection.cs
@@ -8,10 +8,36 @@ namespace woke3
         private readonly GameSession session;
         private readonly GameServer server;
 
-        public GameConnection(GameServer server, GameSession gameSession) : base(server)
+        public GameConnection(GameServer server, GameSession gameSession) : base(server)    
         {
             session = gameSession;
             this.server = server;
+        }
+        
+        protected override void OnConnected()
+        {
+            if (server.ConnectedSessions > 2)
+            {
+                Console.WriteLine("Detect more than 2 clients, disconnecting");
+                SendPacket(PacketType.PKT_END, new byte[] {0}, true);
+                Disconnect();
+            }
+        }
+        
+        protected override void OnDisconnected()
+        {
+            lock (session)
+            {
+                if (session.MatchStarted)
+                {
+                    session.MatchStarted = false;
+                    Console.WriteLine(session.P1Id == Id ? "Player 1 disconnected. Player 2 wins" : "Player 2 disconnected. Player 1 wins");
+
+                    SendPacket(PacketType.PKT_END, new byte[] {0}, true);
+                    server.Session = new GameSession();
+                    Disconnect();
+                }
+            }
         }
 
         protected override void OnReceived(byte[] buffer, long offset, long size)
@@ -31,6 +57,8 @@ namespace woke3
             var type = (PacketType) BitConverter.ToInt32(buffer[0..4]);
             var len = BitConverter.ToInt32(buffer[4..8]);
             var payload = buffer[8..(8 + len)];
+            for (int i = 0; i < 8 + len; ++i) Console.Write(buffer[i] + " ");
+            Console.WriteLine();
             
             switch (type)
             {
@@ -53,6 +81,7 @@ namespace woke3
                             };
                             Console.WriteLine($"Sent {PacketType.PKT_ID} {session.P2} as {nameof(session.P2)}");
                             session.P2Connected = true;
+                            session.P2Id = Id;
                         }
                         else
                         {
@@ -63,25 +92,32 @@ namespace woke3
                             };
                             Console.WriteLine($"Sent {PacketType.PKT_ID} {session.P1} as {nameof(session.P1)}");
                             session.P1Connected = true;
+                            session.P1Id = Id;
+                        }
+
+                        if (session.P1Connected && session.P2Connected)
+                        {
+                            session.MatchStarted = true;
                         }
                     }
                     SendPacket(PacketType.PKT_ID, list.SelectMany(a => a).ToArray());
-                    lock (session.Matrix)
-                    {
-                        SendBoard(session.Matrix);
-                    }
+
                     break;
                 }
                 case PacketType.PKT_SEND:
                 {
+                    Console.WriteLine($"{PacketType.PKT_SEND} received");
                     lock (session)
                     {
-                        var id = BitConverter.ToInt32(payload[0..4]);
+                        Console.WriteLine("Payload : {0}", string.Join(" ", payload.Select(a => a.ToString())));
+                        var id = BitConverter.ToUInt32(payload[0..4]);
+                        Console.WriteLine(id);
                         if (id != session.P1 && id != session.P2)
                         {
                             // let it pass
                             break;
                         }
+
                         var pos = BitConverter.ToInt32(payload[4..8]);
                         var matrix = session.Matrix;
                         var n = matrix.GetLength(0);
@@ -92,18 +128,20 @@ namespace woke3
                             SendError();
                             break;
                         }
-                        if (matrix[row, col] == session.P1 || matrix[row, col] == session.P2 || matrix[row, col] == GameSession.BannedCell)
+
+                        if (matrix[row, col] == session.P1 || matrix[row, col] == session.P2 ||
+                            matrix[row, col] == GameSession.BannedCell)
                         {
                             Console.WriteLine($"{row}, {col} already got marked, rejecting");
                             SendError();
                             break;
                         }
 
-                        matrix[row, col] = id;
+                        matrix[row, col] = (int)id;
                         Console.WriteLine($"Marking {row}, {col} belong to player {id}");
                         var receivePacket = BitConverter.GetBytes(row * n * col);
                         SendPacket(PacketType.PKT_RECEIVE, receivePacket, true);
-                        SendBoard(session.Matrix);
+                        
                     }
 
                     break;
@@ -141,6 +179,12 @@ namespace woke3
             };
             SendPacket(PacketType.PKT_BOARD, payload.SelectMany(a => a).ToArray());
         }
+
+        private void SendReceive(int pos)
+        {
+            var payload = BitConverter.GetBytes(pos);
+            SendPacket(PacketType.PKT_RECEIVE, payload.ToArray());
+        }
         
         private void SendError()
         {
@@ -154,10 +198,11 @@ namespace woke3
                 type != PacketType.PKT_BOARD
                     ? BitConverter.ToString(payload).Replace("-", " ")
                     : string.Join(" ", payload.Select(a => a.ToString())));
+            Console.WriteLine("Payload {0}", string.Join(" ", payload.Select(a => a.ToString())));
             var b = new List<byte[]>
             {
                 BitConverter.GetBytes((int) type),
-                BitConverter.GetBytes(payload.Length),
+                type != PacketType.PKT_END ? BitConverter.GetBytes(payload.Length) : new byte[] {0},
                 payload
             };
             var final = b.SelectMany(a => a).ToArray();
--- GameSession.cs
+++ GameSession.cs
@@ -7,8 +7,12 @@ namespace woke3
         public readonly int[,] Matrix;
         public readonly int P1 = Random.Shared.Next();
         public readonly int P2 = Random.Shared.Next();
+        public Guid P1Id { get; set; }
+        public Guid P2Id { get; set; }
+
         public bool P1Connected { get; set; } = false;
         public bool P2Connected { get; set; } = false;
+        public bool MatchStarted { get; set; } = false;
 
         public const int BannedCell = -2;
 
