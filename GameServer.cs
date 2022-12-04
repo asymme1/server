b976dec9720b92b84794b62cf7a71384e144058e
asymme1 <73091859+asymme1@users.noreply.github.com>
2022-12-04 10:07:25 +0700

    disable

--- GameServer.cs
+++ GameServer.cs
@@ -10,9 +10,9 @@ namespace woke3
 
         public GameServer(IPAddress address, int port, int wsPort) : base(address, port)
         {
-            Session = new GameSession(1, 1, 2, "asasdas");
-            WsServer = new WebsocketServer(address, wsPort);
-            WsServer.Start();
+            // Session = new GameSession(1, 1, 2, "asasdas");
+            // WsServer = new WebsocketServer(address, wsPort);
+            // WsServer.Start();
         }
 
         protected override TcpSession CreateSession()
